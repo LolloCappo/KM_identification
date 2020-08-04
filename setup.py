0652eaeeebc4a6c6d92296dced12b14d460d5634
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-08-04 18:09:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@ with open('README.rst', 'r', encoding='utf8') as f:
 from setuptools import setup, Extension
 from ThermCoeff import __version__
 
-if __name__ == '__main__':
-    setup(
-        name='ThermCoeff',
-        version=__version__,
-        author='Lorenzo Capponi',
-        author_email='lorenzocapponi@outlook.it',
-        description='Module for thermoelastic coefficient identification',
-        url='https://github.com/LolloCappo/ThermCoeff',
-        py_modules='ThermCoeff',
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        install_requires = ['numpy','pyLIA>=0.6']
-    )
+setup(
+    name='ThermCoeff',
+    version=__version__,
+    author='Lorenzo Capponi',
+    author_email='lorenzocapponi@outlook.it',
+    description='Module for thermoelastic coefficient identification',
+    url='https://github.com/LolloCappo/ThermCoeff',
+    py_modules=['ThermCoeff'],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires = ['numpy','pyLIA>=0.6']
+)
